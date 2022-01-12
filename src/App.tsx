1df9cbbb7d05ce1b30a5168ec9f3a66866a4177f
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-01-13 00:29:46 +0900

    add signUp && signIn component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { SiginUp } from "./features/components/sign-up/SIgnUp";
+import { SiginIn } from "./features/components/sign-in/SignIn";
+import { SiginUp } from "./features/components/sign-up/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SiginUp />} />
+        <Route path="/" element={<SiginIn />} />
+        <Route path="/signin" element={<SiginIn />} />
+        <Route path="/signup" element={<SiginUp />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/features/components/sign-up/SignUp.tsx
@@ -0,0 +1,124 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Link,
+  Stack,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import * as Yup from "yup";
+
+const validationScheme = {
+  initialValues: {
+    email: "",
+    password: "",
+  },
+  validationScheme: Yup.object({
+    email: Yup.string().email().required(),
+    password: Yup.string().required().min(8, "8文字以上にしてください"),
+  }),
+  onSubmit: () => {
+    console.log("submit is Done!!");
+  },
+};
+
+export const SiginUp: React.VFC = () => {
+  const formik = useFormik(validationScheme);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      backgroundColor="gray.200"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Heading size="2xl" color="purple.400" mb="32px">
+          Create Account
+        </Heading>
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <form>
+            <Stack
+              spacing={8}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+              borderRadius="8px"
+            >
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none" />
+                  <Input
+                    value={formik.values.email}
+                    type="email"
+                    name="email"
+                    placeholder="email address"
+                    onChange={formik.handleChange}
+                    borderRadius="8px"
+                  />
+                </InputGroup>
+              </FormControl>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    // children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    placeholder="Password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    borderRadius="8px"
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                      {showPassword ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormHelperText textAlign="right">
+                  <Link>forgot password?</Link>
+                </FormHelperText>
+              </FormControl>
+              <Button
+                borderRadius="8px"
+                type="submit"
+                variant="solid"
+                colorScheme="purple"
+                width="full"
+              >
+                Signin
+              </Button>
+            </Stack>
+          </form>
+        </Box>
+      </Stack>
+      <Box>
+        New to use ?{" "}
+        <Link color="purple.500" href="#">
+          Sign Up
+        </Link>
+      </Box>
+    </Flex>
+  );
+};
