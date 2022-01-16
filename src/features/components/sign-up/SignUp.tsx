4a96ef36e0d7d246ce1094a0a1901524f66ad90b
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-01-16 16:54:36 +0900

    Add password validation.

--- src/features/components/sign-up/SignUp.tsx
+++ src/features/components/sign-up/SignUp.tsx
@@ -16,6 +16,7 @@ import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { ErrorMessage } from "../error-message/ErrorMessage";
 
+const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[!-~]{10,}$/;
 const validationScheme = {
   initialValues: {
     username: "",
@@ -27,21 +28,25 @@ const validationScheme = {
     username: Yup.string()
       .required("必須です")
       .min(5, "5文字以上にしてください")
-      .max(8, "8文字以上にしてください"),
+      .max(8, "12文字以下にしてください"),
     email: Yup.string()
       .email("正しい形式のemailを設定してください")
       .required("必須です"),
     password: Yup.string()
       .required("必須です")
       .min(8, "8文字以上にしてください")
-      .max(24, "24文字以下にしてください"),
-    // .matches(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)$/, {
-    //   message: "大文字・小文字・数字を含むパスワードを指定してください",
-    // }),
+      .max(24, "24文字以下にしてください")
+      .matches(passwordRegex, {
+        message: "大文字・小文字・数字を含むパスワードを指定してください",
+      }),
     confirmPassword: Yup.string()
       .required("必須です")
       .min(8, "8文字以上にしてください")
-      .max(24, "24文字以下にしてください"),
+      .max(24, "24文字以下にしてください")
+      .matches(
+        passwordRegex,
+        'message: "大文字・小文字・数字を含むパスワードを指定してください'
+      ),
   }),
   onSubmit: () => {
     console.log("submit is Done!!");
@@ -88,7 +93,6 @@ export const SiginUp: React.VFC = () => {
                 <InputLeftElement pointerEvents="none" />
                 <Input
                   value={formik.values.username}
-                  // type="text"
                   name="username"
                   placeholder="User name"
                   onChange={formik.handleChange}
