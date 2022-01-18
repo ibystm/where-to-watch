bfec0437e07b74a8c4c2c730631b3303fdbc9bc4
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-01-18 23:48:17 +0900

    delete unnecessary.

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import {
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
