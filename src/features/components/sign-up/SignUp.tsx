b7826fb620893b8104078852fa90a8e8514d8e12
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-02-16 23:59:30 +0900

    use formik component in SignUp component.

--- src/features/components/sign-up/SignUp.tsx
+++ src/features/components/sign-up/SignUp.tsx
@@ -10,7 +10,7 @@ import {
   InputRightElement,
   Stack,
 } from "@chakra-ui/react";
-import { useFormik } from "formik";
+import { Form, Formik, FormikErrors } from "formik";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
@@ -18,45 +18,47 @@ import { useHandleFBErrors } from "../../hooks/useHandleFBErrors";
 import { useSignUp } from "../../hooks/useSignUp";
 import { ErrorMessage } from "../error-message/ErrorMessage";
 
+type SignUpValue = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[!-~]{10,}$/;
-const validationScheme = {
-  initialValues: {
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  },
-  validationSchema: Yup.object().shape({
-    username: Yup.string()
-      .required("必須です")
-      .min(5, "5文字以上にしてください")
-      .max(8, "12文字以下にしてください"),
-    email: Yup.string()
-      .email("正しい形式のemailを設定してください")
-      .required("必須です"),
-    password: Yup.string()
-      .required("必須です")
-      .min(8, "8文字以上にしてください")
-      .max(24, "24文字以下にしてください")
-      .matches(passwordRegex, {
-        message: "大文字・小文字・数字を含むパスワードを指定してください",
-      }),
-    confirmPassword: Yup.string()
-      .required("必須です")
-      .min(8, "8文字以上にしてください")
-      .max(24, "24文字以下にしてください")
-      .matches(
-        passwordRegex,
-        'message: "大文字・小文字・数字を含むパスワードを指定してください'
-      ),
-  }),
-  onSubmit: () => {
-    console.log("submit is Done!!");
-  },
+const validationScheme = Yup.object().shape({
+  username: Yup.string()
+    .required("必須です")
+    .min(5, "5文字以上にしてください")
+    .max(8, "12文字以下にしてください"),
+  email: Yup.string()
+    .email("正しい形式のemailを設定してください")
+    .required("必須です"),
+  password: Yup.string()
+    .required("必須です")
+    .min(8, "8文字以上にしてください")
+    .max(24, "24文字以下にしてください")
+    .matches(passwordRegex, {
+      message: "大文字・小文字・数字を含むパスワードを指定してください",
+    }),
+  confirmPassword: Yup.string()
+    .required("必須です")
+    .min(8, "8文字以上にしてください")
+    .max(24, "24文字以下にしてください")
+    .matches(
+      passwordRegex,
+      'message: "大文字・小文字・数字を含むパスワードを指定してください'
+    ),
+});
+
+const initialValues: SignUpValue = {
+  username: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
 };
 
 export const SignUp: React.VFC = () => {
-  const formik = useFormik(validationScheme);
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [submitError, setSubmitError] = useState<string>("");
   const { handleErrorByCodes } = useHandleFBErrors();
@@ -66,18 +68,16 @@ export const SignUp: React.VFC = () => {
   const toSignInPage = () => {
     navigate("/signin");
   };
-  const hasError = () => {
-    const errorObj = formik.errors;
+  const hasError = (errors: FormikErrors<SignUpValue>): boolean => {
     return (
-      errorObj.confirmPassword ||
-      errorObj.email ||
-      errorObj.password ||
-      errorObj.username
+      !!errors.confirmPassword ||
+      !!errors.email ||
+      !!errors.password ||
+      !!errors.username
     );
   };
-  const handleSubmit = async () => {
-    if (hasError()) return;
-    await signUp(formik.values.email, formik.values.password)
+  const onSubmit = async (values: SignUpValue) => {
+    await signUp(values.email, values.password)
       .then(() => {
         navigate("/");
       })
@@ -108,102 +108,118 @@ export const SignUp: React.VFC = () => {
           Create a Account
         </Heading>
         <Box minW={{ base: "90%", md: "468px" }}>
-          <Stack
-            spacing={6}
-            p="1rem"
-            backgroundColor="whiteAlpha.900"
-            boxShadow="md"
-            borderRadius="20px"
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationScheme}
+            onSubmit={onSubmit}
           >
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none" />
-                <Input
-                  value={formik.values.username}
-                  name="username"
-                  placeholder="User name"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
+            {({
+              values,
+              handleSubmit,
+              handleChange,
+              touched,
+              errors,
+              handleBlur,
+            }) => (
+              <Form>
+                <Stack
+                  spacing={6}
+                  p="1rem"
+                  backgroundColor="whiteAlpha.900"
+                  boxShadow="md"
                   borderRadius="20px"
-                />
-              </InputGroup>
-              {formik.touched.username && formik.errors.username && (
-                <ErrorMessage message={formik.errors.username} />
-              )}
-            </FormControl>
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none" />
-                <Input
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  placeholder="email address"
-                  borderRadius="20px"
-                />
-              </InputGroup>
-              {formik.touched.email && formik.errors.email && (
-                <ErrorMessage message={formik.errors.email} />
-              )}
-            </FormControl>
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none" color="gray.300" />
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  placeholder="Password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  borderRadius="20px"
-                />
-                <InputRightElement width="4.5rem">
-                  <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                    {showPassword ? "Hide" : "Show"}
+                >
+                  <FormControl>
+                    <InputGroup>
+                      <InputLeftElement pointerEvents="none" />
+                      <Input
+                        value={values.username}
+                        name="username"
+                        placeholder="User name"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        borderRadius="20px"
+                      />
+                    </InputGroup>
+                    {touched.username && errors.username && (
+                      <ErrorMessage message={errors.username} />
+                    )}
+                  </FormControl>
+                  <FormControl>
+                    <InputGroup>
+                      <InputLeftElement pointerEvents="none" />
+                      <Input
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="email address"
+                        borderRadius="20px"
+                      />
+                    </InputGroup>
+                    {touched.email && errors.email && (
+                      <ErrorMessage message={errors.email} />
+                    )}
+                  </FormControl>
+                  <FormControl>
+                    <InputGroup>
+                      <InputLeftElement pointerEvents="none" color="gray.300" />
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        placeholder="Password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        borderRadius="20px"
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                          {showPassword ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    {errors.password && touched.password && (
+                      <ErrorMessage message={errors.password} />
+                    )}
+                  </FormControl>
+                  <FormControl>
+                    <InputGroup>
+                      <InputLeftElement pointerEvents="none" color="gray.300" />
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        name="confirmPassword"
+                        placeholder="Password"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        borderRadius="20px"
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                          {showPassword ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    {touched.confirmPassword && errors.confirmPassword && (
+                      <ErrorMessage message={errors.confirmPassword} />
+                    )}
+                  </FormControl>
+                  <Button
+                    borderRadius="20px"
+                    type="submit"
+                    variant="solid"
+                    disabled={hasError(errors)}
+                    colorScheme="purple"
+                    width="full"
+                  >
+                    Sign Up
                   </Button>
-                </InputRightElement>
-              </InputGroup>
-              {formik.errors.password && formik.touched.password && (
-                <ErrorMessage message={formik.errors.password} />
-              )}
-            </FormControl>
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement pointerEvents="none" color="gray.300" />
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  name="confirmPassword"
-                  placeholder="Password"
-                  value={formik.values.confirmPassword}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  borderRadius="20px"
-                />
-                <InputRightElement width="4.5rem">
-                  <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                    {showPassword ? "Hide" : "Show"}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              {formik.touched.confirmPassword &&
-                formik.errors.confirmPassword && (
-                  <ErrorMessage message={formik.errors.confirmPassword} />
-                )}
-            </FormControl>
-            <Button
-              borderRadius="20px"
-              type="submit"
-              variant="solid"
-              colorScheme="purple"
-              width="full"
-              onClick={handleSubmit}
-            >
-              Sign Up
-            </Button>
-            {submitError && <ErrorMessage message={submitError} />}
-          </Stack>
+                  {submitError && <ErrorMessage message={submitError} />}
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Stack>
       <Box>
