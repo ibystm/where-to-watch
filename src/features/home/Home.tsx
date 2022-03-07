50a2180f55c687064701600beee58ec18a55c262
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-03-07 23:39:25 +0900

    Modify loading UI to skelton UI

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,13 +1,10 @@
-import { Box, Flex, Spinner } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import React from "react";
-import { useSelector } from "../../store/store";
 import { useFetchConfigs } from "./hooks/useFetchConfigs";
 import { useFetchContents } from "./hooks/useFetchContents";
 import { MainContens } from "./MainContents";
-import { selectLoadingState } from "./slice/contents";
 
 export const Home: React.VFC = () => {
-  const loadingContents = useSelector(selectLoadingState);
   // initial process
   useFetchContents();
   useFetchConfigs();
@@ -19,11 +16,7 @@ export const Home: React.VFC = () => {
   //     console.log(e);
   //   });
   // };
-  return loadingContents ? (
-    <Flex w="100%" h="100%" justifyContent="center" alignItems="center">
-      <Spinner color="purple.500" size="xl" thickness="4px" />
-    </Flex>
-  ) : (
+  return (
     <Box mt="160px" w="100%" h="100%">
       <MainContens />
     </Box>
--- src/features/home/MainContents.tsx
+++ src/features/home/MainContents.tsx
@@ -1,16 +1,27 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "../../store/store";
 import { ContentItem } from "./ContentItem";
-import { selectContents } from "./slice/contents";
+import { selectContents, selectLoadingState } from "./slice/contents";
 
 export const MainContens: React.FC = () => {
   const contents = useSelector(selectContents);
+  const loadingContents = useSelector(selectLoadingState);
   return (
     <SimpleGrid minChildWidth="192px" spacing="24px" marginX="40px">
-      {contents.map((item, idx) => (
-        <ContentItem contentItem={item} key={idx} />
-      ))}
+      {!loadingContents &&
+        contents.map((item, idx) => (
+          <ContentItem contentItem={item} key={idx} />
+        ))}
+      {loadingContents &&
+        [...Array(100)].map((a) => (
+          <Skeleton
+            height="256px"
+            maxW="192px"
+            variant="unstyled"
+            borderRadius="20px"
+          />
+        ))}
     </SimpleGrid>
   );
 };
