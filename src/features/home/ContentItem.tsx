e7daebb8e3bba48b8396cd0368a3075089de6181
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-03-15 00:41:50 +0900

    Modify some content item layout.

--- src/features/home/ContentItem.tsx
+++ src/features/home/ContentItem.tsx
@@ -1,4 +1,4 @@
-import { Button, Image } from "@chakra-ui/react";
+import { Box, Button, Image, Text } from "@chakra-ui/react";
 import { IMAGE_BASE_URL } from "../../apis/constants";
 import { ActualContentData } from "../../types/redux/discovers";
 
@@ -7,7 +7,7 @@ type P = {
 };
 
 export const ContentItem: React.VFC<P> = ({ contentItem }) => {
-  const { title, poster_path } = contentItem;
+  const { title, poster_path, releaseDate } = contentItem;
 
   const pathBuilder = (path: string): string | undefined => {
     const url = sessionStorage.getItem("secureBaseUrl");
@@ -15,13 +15,35 @@ export const ContentItem: React.VFC<P> = ({ contentItem }) => {
   };
 
   return (
-    <Button height="256px" maxW="192px" variant="unstyled" borderRadius="20px">
+    <Button
+      height="256px"
+      maxW="192px"
+      variant="unstyled"
+      borderRadius="20px"
+      display="flex"
+      flexDirection="column"
+      _focus={{ _focus: "none" }}
+    >
       <Image
         src={poster_path ? pathBuilder(poster_path) : undefined}
         alt={title ? title : "movie poster"}
-        boxSize="100%"
+        boxSize="85%"
         borderRadius="inherit"
       />
+      <Box w="100%">
+        <Text
+          fontSize="sm"
+          fontWeight="bold"
+          textOverflow="ellipsis"
+          whiteSpace="nowrap"
+          overflow="hidden"
+        >
+          {title ? title : "タイトルなし"}
+        </Text>
+        <Text fontSize="sm">
+          {releaseDate ? releaseDate : "リリース日未定"}
+        </Text>
+      </Box>
     </Button>
   );
 };
