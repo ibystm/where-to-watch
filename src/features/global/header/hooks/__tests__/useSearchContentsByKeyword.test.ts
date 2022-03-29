3eff0bfd0c1eef5208eb6c74e1edf93c23b3e969
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-03-29 20:59:40 +0900

    Modified: chenged how to mock useDispatch

--- src/features/global/header/hooks/__tests__/useSearchContentsByKeyword.test.ts
+++ src/features/global/header/hooks/__tests__/useSearchContentsByKeyword.test.ts
@@ -1,21 +1,29 @@
 import { renderHook } from "@testing-library/react-hooks";
+import * as reactRedux from "react-redux";
 import { useSearchMoviesByKeyword } from "../useSearchContentsByKeyword";
 
-const mockDispatch = jest.fn();
-jest.mock("react-redux", () => ({
-  useDispatch: () => mockDispatch,
-}));
+const useDispatchMock = jest.spyOn(reactRedux, "useDispatch");
+
+beforeEach(() => {
+  useDispatchMock.mockClear();
+});
 
 describe("useSearchMoviesByKeyword", () => {
   it("handleSubmit関数が、引数が空の文字列の時、returnされること", () => {
+    const dummyDispatch = jest.fn();
+    useDispatchMock.mockReturnValue(dummyDispatch);
+
     const { result } = renderHook(() => useSearchMoviesByKeyword());
     result.current.handleSubmit({ searchName: "" });
-    expect(mockDispatch).not.toHaveBeenCalled();
+    expect(dummyDispatch).not.toHaveBeenCalled();
   });
 
   it("handleSubmit関数が、引数が空でない文字列の時、dispatchが呼ばれること", () => {
+    const dummyDispatch = jest.fn();
+    useDispatchMock.mockReturnValue(dummyDispatch);
+
     const { result } = renderHook(() => useSearchMoviesByKeyword());
     result.current.handleSubmit({ searchName: "aiueo" });
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
+    expect(dummyDispatch).toHaveBeenCalledTimes(1);
   });
 });
