4fdb08374084cd7fedf8a6fc782658c7cc74653e
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-04-18 22:46:43 +0900

    Modified contents display hook.

--- src/features/home/hooks/useDisplayContentsControl.ts
+++ src/features/home/hooks/useDisplayContentsControl.ts
@@ -1,10 +1,6 @@
-import { useEffect, useMemo, useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/store";
+import { useMemo } from "react";
+import { RootState, useSelector } from "../../../store/store";
 import { ActualContentData } from "../../../types/redux/discovers";
-import { searchMovieSelectors } from "../../global/header/selectors/searchMovies";
-import { contentsSelectors } from "../selectors/contents";
-import { discoverTVShowsSelectors } from "../selectors/discoverTVShows";
 
 type useDisplayContentsControlReturnType = {
   results: {
@@ -15,39 +11,18 @@ type useDisplayContentsControlReturnType = {
 
 export const useDisplayContentsControl =
   (): useDisplayContentsControlReturnType => {
-    const isSearchMovieLoading = useSelector(searchMovieSelectors.loadingState);
-    const discoverMovies = useSelector(contentsSelectors.selectContents);
-    const discoverTVShows = useSelector(
-      discoverTVShowsSelectors.discoverTVShows
-    );
-    const searchMovies = useSelector(searchMovieSelectors.searchedMovies);
-    const popularTVs = useSelector(
-      (state: RootState) => state.popularities.tvs
-    );
+    const modeIndex = useSelector((state) => state.contentsMode.modeIndex);
     const popularMovies = useSelector(
       (state: RootState) => state.popularities.movies
     );
-    const isLoadingDiscoverMovies = useSelector(
-      contentsSelectors.selectLoadingState
-    );
-    const isLoadingdiscoverTVShows = useSelector(
-      discoverTVShowsSelectors.isDiscoverTVShowsLoading
-    );
-
-    const [searchedContents, setSearchedContents] = useState<
-      ActualContentData[]
-    >([]);
-
-    useEffect(() => {
-      setSearchedContents(searchMovies);
-    }, [searchMovies]);
+    const popularTVs = useSelector((state) => state.popularities.tvs);
 
     const results = useMemo(
       () => ({
         loading: false,
-        data: popularMovies,
+        data: modeIndex === 0 ? popularMovies : popularTVs,
       }),
-      [popularMovies]
+      [modeIndex, popularMovies, popularTVs]
     );
 
     return {
