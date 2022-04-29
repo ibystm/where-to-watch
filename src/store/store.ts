4a0aeaa9cb2c304d7e800961eed6ba09478043bf
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-04-29 23:57:11 +0900

    Renamed contents reducer name.

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,7 +6,7 @@ import {
 import { reducer as configurationsReducer } from "../features/configurations/slice/configurations";
 import { contentsModeReducer } from "../features/global/header/slice/contentsMode";
 import { searchContentsReducer } from "../features/global/header/slice/searchContents";
-import { reducer as discoverMoviesReducer } from "../features/home/slice/discoverMovies";
+import { reducer as discoverContentsReducer } from "../features/home/slice/discoverMovies";
 import { genresReducer } from "../features/home/slice/genres/index";
 import { reducer as loadingReducer } from "../features/loading/slice/loading";
 import { popularitiesReducer } from "../features/loading/slice/popularities/index";
@@ -17,7 +17,7 @@ export const store = configureStore({
     contentsMode: contentsModeReducer,
     loading: loadingReducer,
     user: userReducer,
-    contents: discoverMoviesReducer,
+    contents: discoverContentsReducer,
     popularities: popularitiesReducer,
     searchContents: searchContentsReducer,
     configurations: configurationsReducer,
