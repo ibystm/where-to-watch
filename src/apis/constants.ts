3a883605142f6ae5e8553c21880b3e6c9cf29531
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-11 00:09:14 +0900

    Add watch provder API uris.

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -23,6 +23,10 @@ export const endPoints = {
     `/movie/popular?${languages.ja}&${API_KEY_QUERY_STRINGS}&region=${region}&page=${page}`,
   getPopularTVs: (region: ISOCodes31661, page: number = 1) =>
     `/tv/popular?${languages.ja}&${API_KEY_QUERY_STRINGS}&region=${region}&page=${page}`,
+  getMovieWatchProvider: (movieId: string) =>
+    `/movie/${movieId}/watch/providers?${API_KEY_QUERY_STRINGS}`,
+  getTVWatchProvider: (tvId: string) =>
+    `/tv/${tvId}/watch/providers?${API_KEY_QUERY_STRINGS}`,
 
   // getUpComingTVs: (page: number = 1) =>
   //   `/tv/upcoming?${languages.ja}&${API_KEY_QUERY_STRINGS}&page=${page}`,
