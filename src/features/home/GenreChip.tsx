8133467de04d286f3ce17493bae67f9020ed8af4
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-21 00:19:44 +0900

    Modified so that genre chips area can be scrolled.

--- src/features/home/GenreChip.tsx
+++ src/features/home/GenreChip.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@chakra-ui/react";
+import { forwardRef } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "../../store/store";
 import { Genre } from "../../types/redux/genres";
@@ -8,28 +9,31 @@ type Props = {
   genre: Genre;
 };
 
-export const GenreChip: React.FC<Props> = ({ genre }) => {
-  const { selectedGenreId } = useSelector((state) => state.contentsMode);
-  const dispatch = useDispatch();
-  const shouldFocus = selectedGenreId === genre.id;
+export const GenreChip = forwardRef<HTMLButtonElement, Props>(
+  ({ genre }, ref) => {
+    const { selectedGenreId } = useSelector((state) => state.contentsMode);
+    const dispatch = useDispatch();
+    const shouldFocus = selectedGenreId === genre.id;
 
-  return (
-    <Button
-      marginY="2"
-      height="12"
-      variant="outline"
-      borderRadius="20px"
-      padding="8px 16px"
-      flexShrink="0"
-      color={shouldFocus ? "white" : "inherit"}
-      backgroundColor={shouldFocus ? "gray.700" : "inherit"}
-      onClick={() => dispatch(contentModeActions.selectGenre(genre.id))}
-      _hover={{
-        backgroundColor: shouldFocus ? "gray.700" : "inherit",
-      }}
-      _focus={{ boxShadow: "none" }}
-    >
-      {genre.name}
-    </Button>
-  );
-};
+    return (
+      <Button
+        ref={ref}
+        marginY="2"
+        height="12"
+        variant="outline"
+        borderRadius="20px"
+        padding="8px 16px"
+        flexShrink="0"
+        color={shouldFocus ? "white" : "inherit"}
+        backgroundColor={shouldFocus ? "gray.700" : "inherit"}
+        onClick={() => dispatch(contentModeActions.selectGenre(genre.id))}
+        _hover={{
+          backgroundColor: shouldFocus ? "gray.700" : "inherit",
+        }}
+        _focus={{ boxShadow: "none" }}
+      >
+        {genre.name}
+      </Button>
+    );
+  }
+);
--- src/features/home/GenreChipsArea.tsx
+++ src/features/home/GenreChipsArea.tsx
@@ -1,23 +1,30 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Flex, IconButton } from "@chakra-ui/react";
+import React, { useRef } from "react";
 import { GenreChip } from "./GenreChip";
 import { useGenreChipsArea } from "./hooks/useGenreChipsArea";
 
 export const GenreChipsArea: React.FC = () => {
   const displayGenres = useGenreChipsArea();
+  const ref = useRef<HTMLDivElement>(null);
 
   return displayGenres.length > 0 ? (
     <Flex w="100%" justify="space-around" alignItems="center" padding="4">
-      <Flex overflowX="hidden" gap="4">
-        {displayGenres.map((item) => (
+      <Flex overflowX="hidden" gap="4" ref={ref}>
+        {displayGenres.map((item, idx) => (
           <GenreChip key={item.id} genre={item} />
         ))}
       </Flex>
+
       <IconButton
         marginLeft="4"
         aria-label="next-content"
         icon={<ChevronRightIcon />}
         fontSize="2xl"
+        onClick={() => {
+          if (ref.current === null) return;
+          ref.current.scrollLeft += 200;
+        }}
       />
     </Flex>
   ) : null;
