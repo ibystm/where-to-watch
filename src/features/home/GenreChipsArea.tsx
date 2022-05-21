17ff26bcde0ca2385fc8b5f085f45b95084fa011
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-21 17:40:32 +0900

    Added resize obsever hook.

--- src/features/home/GenreChipsArea.tsx
+++ src/features/home/GenreChipsArea.tsx
@@ -1,17 +1,25 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Flex, IconButton } from "@chakra-ui/react";
-import React, { useRef } from "react";
+import { useRef, useState } from "react";
 import { GenreChip } from "./GenreChip";
 import { useGenreChipsArea } from "./hooks/useGenreChipsArea";
+import { useResizeObserver } from "./useResizeObserver";
 
 export const GenreChipsArea: React.FC = () => {
   const displayGenres = useGenreChipsArea();
-  const ref = useRef<HTMLDivElement>(null);
+  const genreChipAreaRef = useRef<HTMLDivElement>(null);
+  // const nextButtonRef = useRef<HTMLDivElement>(null);
+  const [genreChipAreaWidth, setGenreChipAreaWidth] = useState(0);
+  const handleResize = (entries: ResizeObserverEntry[]) => {
+    const width = entries[0].contentRect.width;
+    setGenreChipAreaWidth(width);
+  };
+  useResizeObserver([genreChipAreaRef], handleResize);
 
   return displayGenres.length > 0 ? (
     <Flex w="100%" justify="space-around" alignItems="center" padding="4">
-      <Flex overflowX="hidden" gap="4" ref={ref}>
-        {displayGenres.map((item, idx) => (
+      <Flex overflowX="hidden" gap="4" ref={genreChipAreaRef}>
+        {displayGenres.map((item) => (
           <GenreChip key={item.id} genre={item} />
         ))}
       </Flex>
@@ -22,10 +30,14 @@ export const GenreChipsArea: React.FC = () => {
         icon={<ChevronRightIcon />}
         fontSize="2xl"
         onClick={() => {
-          if (ref.current === null) return;
+          if (genreChipAreaRef.current === null) return;
           // scrollの量を取得して、maxの時はright arrowは隠すように修正
-          console.log(`left: ${ref.current.scrollLeft}`);
-          ref.current.scrollLeft += 200;
+          genreChipAreaRef.current.scrollTo({
+            left: genreChipAreaRef.current.scrollLeft + 200,
+            behavior: "smooth",
+          });
+
+          // (overflowした)リストの最後のelementが画面状に見えているかどうかで、判定ができないかで判断
         }}
       />
     </Flex>
---
+++ src/features/home/useResizeObserver.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export const useResizeObserver = (
+  // TODO 型をちゃんt調べる
+  elements: any,
+  callback: (entries: ResizeObserverEntry[]) => any
+) => {
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      callback(entries);
+    });
+
+    for (const elem of elements) {
+      elem.current && resizeObserver.observe(elem.current);
+    }
+
+    return () => resizeObserver.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
