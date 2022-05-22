0ba12cd9956b870a5b755ce24b63311506866ca7
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-22 12:33:31 +0900

    Added comments.

--- src/features/home/GenreChipsArea.tsx
+++ src/features/home/GenreChipsArea.tsx
@@ -1,27 +1,72 @@
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { Flex, IconButton } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GenreChip } from "./GenreChip";
 import { useGenreChipsArea } from "./hooks/useGenreChipsArea";
-import { useResizeObserver } from "./useResizeObserver";
+import {
+  useInterSectionObserver,
+  useResizeObserver,
+} from "./useResizeObserver";
 
 export const GenreChipsArea: React.FC = () => {
   const displayGenres = useGenreChipsArea();
   const genreChipAreaRef = useRef<HTMLDivElement>(null);
-  // const nextButtonRef = useRef<HTMLDivElement>(null);
   const [genreChipAreaWidth, setGenreChipAreaWidth] = useState(0);
   const handleResize = (entries: ResizeObserverEntry[]) => {
     const width = entries[0].contentRect.width;
     setGenreChipAreaWidth(width);
   };
+
   useResizeObserver([genreChipAreaRef], handleResize);
 
+  // const [shouldShowNextButton, setShouldShowButton] = useState(false);
+  const ref = useRef<HTMLButtonElement>(null);
+  const { isIntersecting } = useInterSectionObserver([ref]);
+
+  const genreChipElements = displayGenres.map((item, idx) => {
+    const isLastItem = idx === displayGenres.length - 1;
+
+    return (
+      <GenreChip
+        key={item.id}
+        genre={item}
+        ref={isLastItem ? ref : undefined}
+      />
+    );
+  });
+
+  const scrollNext = (): void => {
+    if (genreChipAreaRef.current === null) return;
+    // scrollの量を取得して、maxの時はright arrowは隠すように修正
+    genreChipAreaRef.current.scrollTo({
+      left: genreChipAreaRef.current.scrollLeft + 200,
+      behavior: "smooth",
+    });
+
+    // (overflowした)リストの最後のelementが画面状に見えているかどうかで、判定ができないかで判断
+  };
+
+  useEffect(() => {
+    genreChipAreaRef.current &&
+      genreChipAreaRef.current.addEventListener("scroll", () => {
+        // ちゃんと理解は必要そうだけど、genreChipsAreaWidth + element.scrollLeft >= scrollWidthになった時に、
+        // フラグを変えればよさそう！！！
+        console.log({ scrollWidth: genreChipAreaRef.current?.scrollWidth });
+        console.log({ scrollLeft: genreChipAreaRef.current?.scrollLeft });
+      });
+  }, []);
+  console.log("genreChipAreaWidth:", genreChipAreaWidth);
+
   return displayGenres.length > 0 ? (
-    <Flex w="100%" justify="space-around" alignItems="center" padding="4">
-      <Flex overflowX="hidden" gap="4" ref={genreChipAreaRef}>
-        {displayGenres.map((item) => (
-          <GenreChip key={item.id} genre={item} />
-        ))}
+    <Flex
+      w="100%"
+      justify="space-around"
+      alignItems="center"
+      padding="4"
+      backgroundColor="red"
+    >
+      <Flex overflowX="auto" gap="4" ref={genreChipAreaRef}>
+        {genreChipElements}
       </Flex>
 
       <IconButton
@@ -29,16 +74,7 @@ export const GenreChipsArea: React.FC = () => {
         aria-label="next-content"
         icon={<ChevronRightIcon />}
         fontSize="2xl"
-        onClick={() => {
-          if (genreChipAreaRef.current === null) return;
-          // scrollの量を取得して、maxの時はright arrowは隠すように修正
-          genreChipAreaRef.current.scrollTo({
-            left: genreChipAreaRef.current.scrollLeft + 200,
-            behavior: "smooth",
-          });
-
-          // (overflowした)リストの最後のelementが画面状に見えているかどうかで、判定ができないかで判断
-        }}
+        onClick={scrollNext}
       />
     </Flex>
   ) : null;
--- src/features/home/useResizeObserver.ts
+++ src/features/home/useResizeObserver.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const useResizeObserver = (
   // TODO 型をちゃんt調べる
@@ -17,4 +17,34 @@ export const useResizeObserver = (
     return () => resizeObserver.disconnect();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+};
+
+export const useInterSectionObserver = (
+  // TODO 型をちゃんt調べる
+  elements: any,
+  root?: any
+): typeof res => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        setIsIntersecting(entries[0].isIntersecting);
+      },
+      root ? { root } : undefined
+    );
+
+    for (const elem of elements) {
+      elem.current && intersectionObserver.observe(elem.current);
+    }
+
+    return () => intersectionObserver.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const res = {
+    isIntersecting,
+  };
+
+  return res;
 };
