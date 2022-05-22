bc5d468a12624bc5d2f7141fd1abe2abac540044
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-22 20:12:57 +0900

    Modified right arrow effect in genre chip area

--- src/features/home/hooks/useGenreChipsArea.ts
+++ src/features/home/hooks/useGenreChipsArea.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { commonDictionaries } from "../../../commons/constants/dictionaries";
 import { useSelector } from "../../../store/store";
 import { ModeType } from "../../../types/redux/contentsMode";
@@ -12,13 +13,51 @@ const popular: Genre = {
   name: commonDictionaries.popular,
 };
 
-export const useGenreChipsArea = (): typeof displayGenres => {
+export const useGenreChipsArea = (
+  genreChipAreaRef: HTMLDivElement | null
+): typeof res => {
   const modeIndex = useSelector((s) => s.contentsMode.modeIndex);
   const movieGenres = useSelector(movieGenresSelector.selectAll);
   const tvGenres = useSelector(tvGenresSelector.selectAll);
-
   const genres = modeIndex === ModeType.Movie ? movieGenres : tvGenres;
-
   const displayGenres = [popular, ...genres];
-  return displayGenres;
+  const [shouldShowNextButton, setShouldShowButton] = useState(true);
+  const [genreChipAreaWidth, setGenreChipAreaWidth] = useState(0);
+  const handleResize = (entries: ResizeObserverEntry[]) => {
+    const width = entries[0].contentRect.width;
+    setGenreChipAreaWidth(width);
+  };
+  const scrollNext = (): void => {
+    if (genreChipAreaRef === null) return;
+    genreChipAreaRef.scrollTo({
+      left: genreChipAreaRef.scrollLeft + 200,
+      behavior: "smooth",
+    });
+  };
+  useEffect(() => {
+    if (genreChipAreaRef === null) return;
+    genreChipAreaRef.addEventListener("scroll", () => {
+      const currentAreaElement = genreChipAreaRef;
+      if (currentAreaElement === null) return;
+
+      if (
+        // scrollが最後まで行ったとき
+        genreChipAreaWidth + currentAreaElement.scrollLeft >=
+        currentAreaElement.scrollWidth
+      ) {
+        setShouldShowButton(false);
+      } else {
+        setShouldShowButton(true);
+      }
+    });
+  }, [genreChipAreaRef, genreChipAreaWidth]);
+
+  const res = {
+    displayGenres,
+    shouldShowNextButton,
+    handleResize,
+    scrollNext,
+  };
+
+  return res;
 };
