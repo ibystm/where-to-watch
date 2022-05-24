782b7af4157b74d29ff00424acff85aeaaa2a216
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-24 17:17:08 +0900

    Removed unncecessary file.

--- src/features/home/selectors/discoverTVShows.ts
+++
@@ -1,8 +0,0 @@
-// import { RootState } from "../../../store/store";
-// export const discoverTVShowsSelectors = {
-//   discoverTVShows: (state: RootState) => state.discoverTVShows.data,
-//   isDiscoverTVShowsLoading: (state: RootState) =>
-//     state.discoverTVShows.loading.isProcessing,
-// };
-// 念の為残しておく
-export {};
