eeb9844e419b7a51c596cba83c50be907b30c415
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-05-29 23:50:58 +0900

    Removed unnecessary lines.

--- src/features/global-top/header/hooks/useSearchContentsByKeyword.ts
+++ src/features/global-top/header/hooks/useSearchContentsByKeyword.ts
@@ -6,28 +6,24 @@ export type SearchMovieFormValues = {
   searchName: string;
 };
 
-type UseSearchContentsByKeywordReturnType = {
-  handleSubmit: (values: SearchMovieFormValues) => void;
-};
+export const useSearchMoviesByKeyword = (): typeof result => {
+  const dispatch = useDispatch();
+  const modeIndex = useSelector((state) => state.contentsMode.modeIndex);
 
-export const useSearchMoviesByKeyword =
-  (): UseSearchContentsByKeywordReturnType => {
-    const dispatch = useDispatch();
-    const modeIndex = useSelector((state) => state.contentsMode.modeIndex);
-
-    const handleSubmit = (values: SearchMovieFormValues): void => {
-      if (values.searchName === "") {
-        console.log("No search movie name parameter");
-        return;
-      }
-      if (modeIndex === ModeType.Movie) {
-        dispatch(actions.searchMovie(values.searchName));
-      } else {
-        dispatch(actions.searchTV(values.searchName));
-      }
-    };
-
-    return {
-      handleSubmit,
-    };
+  const handleSubmit = (values: SearchMovieFormValues): void => {
+    if (values.searchName.length === 0) {
+      return;
+    }
+    if (modeIndex === ModeType.Movie) {
+      dispatch(actions.searchMovie(values.searchName));
+    } else {
+      dispatch(actions.searchTV(values.searchName));
+    }
   };
+
+  const result = {
+    handleSubmit,
+  };
+
+  return result;
+};
