37ddfc0fd4383a23f20551388e02420226da2f01
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-06-06 00:45:11 +0900

    Modified useDropDownMenu hook.

--- src/features/global-top/dropdown-menu/useDropdownMenu.tsx
+++ src/features/global-top/dropdown-menu/useDropdownMenu.tsx
@@ -5,15 +5,19 @@ import { useSelector } from "../../../store";
 export const useDropDownMenu = (): typeof result => {
   const navigate = useNavigate();
   const user = useSelector((s) => s.user);
+
   const createMenuItems = (): JSX.Element => {
     return (
       <MenuList>
-        <MenuItem>サインイン</MenuItem>
+        {user.id === null && (
+          <MenuItem onClick={() => navigate("/signin")}>サインイン</MenuItem>
+        )}
         <MenuItem onClick={() => navigate("/about")}>
           Where to watchについて
         </MenuItem>
         <MenuItem onClick={() => navigate("lisence")}>Lisence</MenuItem>
         {typeof user.id === "string" && <MenuItem>アカウント</MenuItem>}
+        {typeof user.id === "string" && <MenuItem>サインアウト</MenuItem>}
       </MenuList>
     );
   };
