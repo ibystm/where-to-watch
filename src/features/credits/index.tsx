578236712b88973b7f14647a2e80dd038c3b8c55
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-06-06 22:55:10 +0900

    Added icon.

--- src/features/credits/index.tsx
+++ src/features/credits/index.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Image } from "@chakra-ui/react";
+import TBDBLogo from "../../../src/assets/images/tmdbLogo.svg";
 
 export const Credits: React.FC = () => {
   return (
@@ -7,6 +8,14 @@ export const Credits: React.FC = () => {
       display="flex"
       flexDirection="column"
       justifyContent="center"
-    ></Box>
+      alignItems="center"
+    >
+      <Heading as="h1" size="md" p="4">
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+      </Heading>
+      <Box>
+        <Image src={TBDBLogo} alt="tmdb image" width="80px" />
+      </Box>
+    </Box>
   );
 };
