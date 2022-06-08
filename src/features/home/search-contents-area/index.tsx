1ea8e2489fc3db3fa0f6d76677a003d9e1a58b7f
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-06-08 20:19:50 +0900

    Implemented empty ui in search screen.

--- src/features/home/search-contents-area/index.tsx
+++ src/features/home/search-contents-area/index.tsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { ContentItem } from "../content-item";
 import { ContentDetailModal } from "../contents-detail-modal";
 import { SkeltonContentItem } from "../skelton-content-item";
@@ -23,11 +23,20 @@ export const SearchContentsArea: React.FC = () => {
   return (
     <>
       <Box marginY="24px">
-        <Box marginLeft="32px">
-          <Text fontSize="2xl" fontWeight="bold" color="gray.800">
-            {`${searchedKeyword} の検索結果`}
-          </Text>
-        </Box>
+        {contents.length > 0 ? (
+          <Box marginLeft="32px">
+            <Text fontSize="2xl" fontWeight="bold" color="gray.800">
+              {`${searchedKeyword} の検索結果`}
+            </Text>
+          </Box>
+        ) : (
+          <Flex justifyContent="center" pt="100px">
+            <Text fontSize="xl">
+              {`${searchedKeyword}に一致する結果はありませんでした。`}
+            </Text>
+          </Flex>
+        )}
+
         <SimpleGrid minChildWidth="240px" spacing="24px" marginTop="8px">
           {loading
             ? dummyLoadingSkeltons
