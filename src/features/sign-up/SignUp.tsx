8a164542ae964111758b75315d4a999c34b85c1f
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-07-24 11:52:58 +0900

    Extract yup schema from component file.

--- src/features/sign-up/SignUp.tsx
+++ src/features/sign-up/SignUp.tsx
@@ -15,9 +15,9 @@ import {
 import { Form, Formik, FormikErrors, FormikProps } from "formik";
 import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import * as Yup from "yup";
 import { ErrorMessage } from "../error-message/ErrorMessage";
 import { useHandleFBErrors } from "../sign-in/useHandleFBErrors";
+import { validationScheme } from "./schema";
 import { useSignUp } from "./useSignUp";
 
 type SignUpValue = {
@@ -26,33 +26,6 @@ type SignUpValue = {
   password: string;
   confirmPassword: string;
 };
-
-const passwordRegex = /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])/;
-const validationScheme = Yup.object().shape({
-  userName: Yup.string()
-    .required("必須です")
-    .min(5, "5文字以上にしてください")
-    .max(8, "12文字以下にしてください"),
-  email: Yup.string()
-    .email("正しい形式のemailを設定してください")
-    .required("必須です"),
-  password: Yup.string()
-    .required("必須です")
-    .min(8, "8文字以上にしてください")
-    .max(24, "24文字以下にしてください")
-    .matches(passwordRegex, {
-      message: "大文字・小文字・数字を含むパスワードを指定してください",
-    }),
-  confirmPassword: Yup.string()
-    .required("必須です")
-    .min(8, "8文字以上にしてください")
-    .max(24, "24文字以下にしてください")
-    .matches(
-      passwordRegex,
-      "大文字・小文字・数字を含むパスワードを指定してください"
-    )
-    .oneOf([Yup.ref("password")], "パスワードが一致しません"),
-});
 
 const initialValues: SignUpValue = {
   userName: "",
---
+++ src/features/sign-up/schema.ts
@@ -0,0 +1,27 @@
+import * as Yup from "yup";
+const passwordRegex = /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])/;
+export const validationScheme = Yup.object().shape({
+  userName: Yup.string()
+    .required("必須です")
+    .min(5, "5文字以上にしてください")
+    .max(8, "12文字以下にしてください"),
+  email: Yup.string()
+    .email("正しい形式のemailを設定してください")
+    .required("必須です"),
+  password: Yup.string()
+    .required("必須です")
+    .min(8, "8文字以上にしてください")
+    .max(24, "24文字以下にしてください")
+    .matches(passwordRegex, {
+      message: "大文字・小文字・数字を含むパスワードを指定してください",
+    }),
+  confirmPassword: Yup.string()
+    .required("必須です")
+    .min(8, "8文字以上にしてください")
+    .max(24, "24文字以下にしてください")
+    .matches(
+      passwordRegex,
+      "大文字・小文字・数字を含むパスワードを指定してください"
+    )
+    .oneOf([Yup.ref("password")], "パスワードが一致しません"),
+});
