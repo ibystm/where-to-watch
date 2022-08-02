38b7496372b38756af743b3c96acd61348d0584f
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-08-03 00:05:28 +0900

    Refactored firestore helper

--- src/features/my-profile/useMyProfile.ts
+++ src/features/my-profile/useMyProfile.ts
@@ -1,26 +1,16 @@
-import {
-  collection,
-  CollectionReference,
-  doc,
-  DocumentReference,
-  getDocs,
-} from "firebase/firestore";
+import { collection, CollectionReference } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useDocument } from "react-firebase-hooks/firestore";
 import { collectionList } from "../../db/constants/index";
 import db from "../../db/index";
 import { useSelector } from "../../store";
 import { FirestoreUser } from "../../types/db/firestoreTypesUsers";
-import { extractFirestoreDocsFromQS } from "../../utils/firebase/extractFirestoreDocsFromSnapshot";
+import { getDocs } from "../../utils/firebase/firestore/documentsHelper";
 
 type AccountInfoState = {
   value: FirestoreUser | null;
   loading: boolean;
   error: any;
 };
-
-// 一旦固定ID
-const USER_ID = "T4OYfwSi8uvbXN9BjJRO";
 
 const usersRef = collection(
   db,
@@ -29,9 +19,6 @@ const usersRef = collection(
 
 export const useAccountInfo = (): typeof res => {
   const userId = useSelector((s) => s.user.id);
-  const [document, loading, error] = useDocument<FirestoreUser>(
-    doc(usersRef, USER_ID) as DocumentReference<FirestoreUser>
-  );
   const [accountData, setAccountData] = useState<AccountInfoState>({
     value: null,
     loading: false,
@@ -39,10 +26,8 @@ export const useAccountInfo = (): typeof res => {
   });
 
   useEffect(() => {
-    // getDocsをラップしていい感じにできそう・・・
     if (userId === null) return;
-    getDocs<FirestoreUser>(usersRef)
-      .then(extractFirestoreDocsFromQS)
+    getDocs(usersRef)
       .then((docs) => {
         const doc = docs.find((d) => d.userId === userId);
         if (typeof doc === "undefined") return;
@@ -50,13 +35,17 @@ export const useAccountInfo = (): typeof res => {
           ...pre,
           value: doc,
         }));
+      })
+      .catch((e) => {
+        setAccountData((pre) => ({
+          ...pre,
+          error: e,
+        }));
       });
-  }, [userId, document]);
+  }, [userId]);
 
   const res = {
     accountData,
-    loading,
-    error,
   };
   return res;
 };
