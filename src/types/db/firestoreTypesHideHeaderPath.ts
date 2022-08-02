860b2cdfd2fa4d4867d4f0d4ef65ac222c4b25cf
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-08-03 00:21:57 +0900

    Added new firestore document data type

---
+++ src/types/db/firestoreTypesHideHeaderPath.ts
@@ -0,0 +1,6 @@
+import { FirestoreTypesCommon } from "./firestoreTypesCommon";
+
+export interface FirestoreTypesHideHeaderPath extends FirestoreTypesCommon {
+  name: string;
+  enabled: boolean;
+}
