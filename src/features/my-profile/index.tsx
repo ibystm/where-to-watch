221359dabc841df9b3a9a534293cb61cb4a17475
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-08-13 19:59:36 +0900

    Added: user page template.

--- src/features/my-profile/index.tsx
+++ src/features/my-profile/index.tsx
@@ -1,8 +1,50 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
 import { useAccountInfo } from "./useMyProfile";
 
 // TODO
 export const MyProfile: React.FC = () => {
-  useAccountInfo();
-  return <Box></Box>;
+  const { accountData } = useAccountInfo();
+  const { colorMode } = useColorMode();
+  const { value } = accountData;
+  return (
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Box
+        w="60%"
+        h="85%"
+        borderRadius="20px"
+        boxShadow="lg"
+        borderWidth="1px"
+        borderStyle="solid"
+        borderColor={colorMode === "dark" ? "gray.600" : undefined}
+      >
+        <Heading p="8">アカウント情報</Heading>
+        <Box p="8">
+          <Flex p="4" gap="96px">
+            <Text fontSize="large" fontWeight="bold">
+              ユーザーネーム
+            </Text>
+            <Text fontSize="large" fontWeight="bold">
+              {value?.name ?? "なし"}
+            </Text>
+          </Flex>
+          <Flex p="4" gap="96px">
+            <Text fontSize="large" fontWeight="bold">
+              メールアドレス
+            </Text>
+            <Text fontSize="large" fontWeight="bold">
+              {value?.email ?? "なし"}
+            </Text>
+          </Flex>
+          <Flex p="4" gap="96px">
+            <Text fontSize="large" fontWeight="bold">
+              アカウント登録日
+            </Text>
+            <Text fontSize="large" fontWeight="bold">
+              {value?.createdAt ?? "不明"}
+            </Text>
+          </Flex>
+        </Box>
+      </Box>
+    </Flex>
+  );
 };
