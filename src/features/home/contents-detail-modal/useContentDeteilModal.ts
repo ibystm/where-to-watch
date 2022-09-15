1fbadee54ab9d0d92a2f3cf83c7242cc57687556
Kosuke Takahashi <rhcp.kdog@gmail.com>
2022-09-16 00:28:57 +0900

    Modified: show toast when added bookmark.

--- src/features/home/contents-detail-modal/useContentDeteilModal.ts
+++ src/features/home/contents-detail-modal/useContentDeteilModal.ts
@@ -1,3 +1,4 @@
+import { useToast } from "@chakra-ui/react";
 import { addBookMark as addBookMarkToFirestore } from "../../../db/firestore/users/bookmarks";
 import { useSelector } from "../../../store";
 import { BookMark } from "../../../types/db/firestoreTypesBookMarks";
@@ -7,12 +8,25 @@ type Param = Omit<BookMark, "modeIndex">;
 export const useContentDeteilModal = (): typeof result => {
   const userId = useSelector((s) => s.user.id);
   const modeIndex = useSelector((s) => s.contentsMode.modeIndex);
+  const toast = useToast();
 
   const handleClickBookMark = async (param: Param) => {
     if (userId === null) return;
     await addBookMarkToFirestore(userId, {
       modeIndex,
       ...param,
+    });
+    toast({
+      // Added to your bookmark!
+      title: "ブックマークに追加しました！",
+      duration: 5000,
+      isClosable: true,
+      status: "success",
+      position: "top",
+      containerStyle: {
+        width: "300px",
+        maxWidth: "500px",
+      },
     });
   };
 
